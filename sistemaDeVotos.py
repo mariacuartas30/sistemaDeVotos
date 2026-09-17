f65e474b360213ac869a0be5283604dac948bbc8
Evelin2405 <ep5781665@gmail.com>
2026-09-17 13:24:43 -0500

    feat: agregar registro de votos y validar votos duplicados

--- sistemaDeVotos.py
+++ sistemaDeVotos.py
@@ -1,1 +1,8 @@
 votos ={}
+def registrar_voto(votos, persona, candidato):
+    if persona in votos:
+        print("Esta persona ya realizó su voto.")
+        return
+
+    votos[persona] = candidato
+    print("Voto registrado correctamente.")
