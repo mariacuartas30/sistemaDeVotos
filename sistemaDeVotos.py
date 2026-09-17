9c791ae76c124d0911d8fdb58d01227199d4bd9c
Evelin2405 <ep5781665@gmail.com>
2026-09-17 13:29:51 -0500

    feat: agregar reinicio de votacion y guardar historial

--- sistemaDeVotos.py
+++ sistemaDeVotos.py
@@ -1,1 +1,19 @@
 votos ={}
+def reiniciar_votacion(votos):
+    if not votos:
+        print("No hay votos para reiniciar.")
+        return {}
+
+    with open("historial_votaciones.txt", "a", encoding="utf-8") as archivo:
+        archivo.write("=== HISTORIAL DE VOTACIÓN ===\n")
+
+        for persona, candidato in votos.items():
+            archivo.write(f"{persona} -> {candidato}\n")
+
+        archivo.write("\n")
+
+    print("Votación reiniciada correctamente.")
+    print("El historial fue guardado.")
+
+    return {}
+    
